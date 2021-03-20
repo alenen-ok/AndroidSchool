6bf622edf5b8459f8ed100925809c04dc9c5efc4
Alena Kominch <alena_kominch@mail.ru>
2021-03-20 18:17:38 +0300

    Simple tasks commit + CSV + dropLastUntil

--- app/src/main/java/ru/skillbranch/kotlinexample/extentions/Iterable.kt
+++ app/src/main/java/ru/skillbranch/kotlinexample/extentions/Iterable.kt
@@ -1,4 +1,7 @@
 package ru.skillbranch.kotlinexample.extentions
 
-class Iterable {
+fun <T> List<T>.dropLastUntil(predicate: (T) -> Boolean): List<T> {
+    val indexOfTruePredicate = this.indexOfLast(predicate)
+    val size = this.size
+    return this.dropLast(size - indexOfTruePredicate)
 }
